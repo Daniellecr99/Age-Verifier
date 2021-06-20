6b381710ca8c4d30001d57315617d26c3fd34437
“Danielle” <Daniellecr99@gmail.com>
2021-06-20 03:52:59 -0600

    form

--- orderform.js
+++ orderform.js
@@ -5,7 +5,8 @@ function orderForm() {
     var order = document.getElementById("ordernumber").value; //$23.50 each
     var order2 = document.getElementById("ordernumber2").value; //$21.50 each
     var order3 = document.getElementById("ordernumber3").value; //$20 each
-   document.getElementById("results").innerHTML=(order * 23.50 + order2 * 21.50 + order3 * 20 );
+   document.getElementById('results').innerHTML= (order * 23.50 + order2 * 21.50 + order3 * 20 );
 }
+orderForm();
 
 //+ " tinctures" + " shipped to " + addy + " for " + myName
