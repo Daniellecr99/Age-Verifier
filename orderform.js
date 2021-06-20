6279ea9d35f934b2b6811d4e3df5ca57b6daa487
“Danielle” <Daniellecr99@gmail.com>
2021-06-20 02:36:57 -0600

    changes

--- orderform.js
+++ orderform.js
@@ -3,6 +3,6 @@ function orderForm() {
     var myName = document.getElementById("ordername").value;
     var addy = document.getElementById("orderaddress").value;
     var order = document.getElementById("ordernumber").value;
-    document.write(order + " tinctures" + " shipped to " + addy + " for " + myName);
+    document.getElementById("results").innerHTML = order + " tinctures" + " shipped to " + addy + " for " + myName;
 }
 
