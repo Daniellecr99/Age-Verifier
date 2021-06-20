11ad37a8b37a015cc37c35d683e3cf16c2782975
“Danielle” <Daniellecr99@gmail.com>
2021-06-20 02:41:51 -0600

    results

--- orderform.js
+++ orderform.js
@@ -3,6 +3,6 @@ function orderForm() {
     var myName = document.getElementById("ordername").value;
     var addy = document.getElementById("orderaddress").value;
     var order = document.getElementById("ordernumber").value;
-    document.getElementById("results").innerHTML = order + " tinctures" + " shipped to " + addy + " for " + myName;
+    document.getElementById("results").textContent = order + " tinctures" + " shipped to " + addy + " for " + myName;
 }
 
